530f1572fec20d87649088a19f48ca9be6834c22
typetypee <kljung171212@gmail.com>
2022-11-19 21:26:58 -0600

    Fixed answer system
    Got rid of some random white space.
    Fixed the reason the button hover wasn't working.
    Fixed the text advancing a step too far after answering a question.

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 var textBox = document.getElementById("text");
 var textName = document.getElementById("name");
 textBox.innerHTML = "AHH";
-var answerBoxes = document.getElementsByClassName("answer");
-
-
-
+var answerBoxes = Array.prototype.slice.call(document.getElementsByClassName("answer"));
 
 var data = {
   data: "",
@@ -20,8 +17,6 @@ importData("json/speech.json", function(json){
 
 })
 
-
-
 function findLabel(label) {
     var index = data.story.findIndex(function(obj){
           return obj.label === label;
@@ -33,11 +28,9 @@ function advanceText() {
   var currentStep = data.story[data.currentLine]; //the current line being displayed in the story
 
   if(data.currentLine < data.story.length) { //if the story is not over yet
-
     if(undefined !== currentStep.n) { //if the name of the current dialogue is not undefined...
       textName.innerText = currentStep.n; //...set the name parameter of the textbox as current name
     }
-
     if(undefined !== currentStep.m) { //if the "message" of the current dialogue is not undefined...
       textBox.innerText = currentStep.m; //...set the content parameter of the textbox as the current content
 
@@ -46,9 +39,8 @@ function advanceText() {
       else { //the dialogue 1 has no "next" parameter
         data.currentLine++; //just go to the next dialogue in the story
       }
-
     } else if (undefined !== currentStep.question) { //the dialogue is not a "messasge", but a "question"
-      if(data.isQuestion === true) { //the isQuestion state has been activated. set up stuff that happens after the player answers the question
+      if(data.isQuestion === true) { //the isQuestion state has already been activated. change the text to the response to the player's answer
         data.currentLine = findLabel(currentStep.answers[data.option].next);
         currentStep = data.story[data.currentLine];
         textBox.innerText = currentStep.m;
@@ -56,15 +48,14 @@ function advanceText() {
           textName.innerText = currentStep.n; //...display it
         }
         //make the answer box disappear
-
         document.getElementById("answer-container").style.display = "none";
-        for(var q = 0; q < answerBoxes.length; q++) {
+        for(var q = 0; q < answerBoxes.length - 1; q++) {
           answerBoxes[q].style.display = "none";
         }
 
-        data.isQuestion = false;
-        data.currentLine++;
-      } else { //erm....it's a question but the variable has not been activated. set up the question
+        data.isQuestion = false; //question process is over. set it to false now
+
+      } else { //erm....it's a question but the variable has not been activated yet. set up the question so the player can respond
         data.isQuestion = true;
 
         //display question
@@ -73,7 +64,7 @@ function advanceText() {
         //display answers
         document.getElementById("answer-container").style.display = "block";
 
-        for(var w = 0; w < answerBoxes.length; w++) { //hide all the answers first...
+        for(var w = 0; w < answerBoxes.length - 1; w++) { //hide all the answers first...
           answerBoxes[w].style.display = "none";
         }
 
@@ -85,8 +76,8 @@ function advanceText() {
 
     }
   }
-
 }
+
 window.addEventListener("keydown", function(e){ //if a key was pressed
   e.preventDefault();
 
@@ -114,18 +105,16 @@ window.addEventListener("click", function(e) {
   if(data.isQuestion === false) advanceText();
 })
 
-function loop() { //loops all functions every few frames
-
-  if(data.isQuestion === true) {
-    for(var i = 0; i < answerBoxes.length; i++) {
-
-      answerBoxes[i].onclick = function(){
-        data.option = i;
-        console.log(data.option);
-        advanceText();
-      };
+for(var i = 0; i < answerBoxes.length - 1; i++) {
+  answerBoxes[i].addEventListener("click", function() {
+    if(data.isQuestion === true) {
+      data.option = answerBoxes.indexOf(this);
+      advanceText();
     }
-  }
+  });
+}
+
+function loop() { //loops all functions every few frames
 
   window.requestAnimationFrame(loop);
 }

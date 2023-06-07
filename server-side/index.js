be226eb2cc65c262a53fdf66ca00fa1e7c650030
typetypee <79076104+typetypee@users.noreply.github.com>
2023-06-07 00:29:38 +0000

    working on get stuff

--- server-side/index.js
+++ server-side/index.js
@@ -1,20 +1,28 @@
-var http = require('http');
+//var http = require('http');
 
-const server = http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hello World!');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + "/client-side"));
+
+app.post("/exportJSON", function(req, res) {
+  res.send(req.body.name + " was received!");
 })
 
-server.listen(3000);
+app.listen(3000);
 
 
+
+/**  
 const fs = require('fs');
 var infoo;
+
 
 var data = fs.readFileSync("json/test-speech.json", {encoding: "utf-8", flag: "r+"});
 
 console.log(data);
-
+**/
 
 /**  
 fs.writeFileSync("json/test-speech.json", infoo, {flag: 'r+'}, function(err) {
